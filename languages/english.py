d6bf69d9ab25b9efb48a4cc640bd35b0d8cda66f
chrissypants77 <104855434+chrissypants77@users.noreply.github.com>
2022-05-09 01:51:30 +0200

    Add files via upload

---
+++ languages/english.py
@@ -0,0 +1,14 @@
+def text():
+    return {"pre_new_game": ["Back", "Your name ~", "Start", "Choose your class"],
+            "game_menu": ["World", "Endless", "You died!", "Save", "Settings"],
+            "class": ["Wizard", "Hp", "Max_hp", "Damage"],
+            "check_new_name": ["One of your entries was Incorrect"],
+            "endless": ["What do you want to do?", ["Attack", "Heal"], "Confirm", "Enemy"],
+            "check_interaction": ["You don't have a healing potion"],
+            "world_level_options": ["World ", "Level: "],
+            "game_settings": ["Back", "Choose your language", "Save", "Saved"],
+
+            "return_player_info": ["Class", "Hp", "Damage", "Level", "Xp", " of "],
+
+            "return_info": ["Name", "Hp", "Damage"]
+            }
---
+++ story_mode.py
@@ -0,0 +1,10 @@
+def tutorial_1(root, language_dictionary):
+    pass
+
+
+def level_1_1(root, language_dictionary):
+    pass
+
+
+def level_1_2(root, language_dictionary):
+    pass
